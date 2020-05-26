09d3471a9d944451624ebb6c5c55abcdca40541b
Christopher Osthues <christopher.osthues@gmail.com>
2020-05-26 20:14:36 +0200

    Add ui test id project

---
+++ Content/Xamarin.UITests.TestIds/TestIds.cs
@@ -0,0 +1,9 @@
+﻿namespace Xamarin.UITests.TestIds
+{
+    public static class TestIds
+    {
+        public const string BaseContinueButton = "base_continue_button";
+        public const string BaseLoadingIndicator = "base_loading_indicator";
+        public const string BaseScrollView = "base_scroll_view";
+    }
+}
